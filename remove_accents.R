0a741a532714f08dde068960215bdcc96a81d7c0
felipefb-barros <95257997+felipefb-barros@users.noreply.github.com>
2021-12-13 09:03:53 -0300

    Add files via upload

---
+++ remove_accents.R
@@ -0,0 +1,46 @@
+library(dplyr)
+library("writexl")
+
+rm_accent <- function(str,pattern="all") {
+  if(!is.character(str))
+    str <- as.character(str)
+  
+  pattern <- unique(pattern)
+  
+  if(any(pattern=="Ç"))
+    pattern[pattern=="Ç"] <- "ç"
+  
+  symbols <- c(
+    acute = "áéíóúÁÉÍÓÚýÝ",
+    grave = "àèìòùÀÈÌÒÙ",
+    circunflex = "âêîôûÂÊÎÔÛ",
+    tilde = "ãõÃÕñÑ",
+    umlaut = "äëïöüÄËÏÖÜÿ",
+    cedil = "çÇ"
+  )
+  
+  nudeSymbols <- c(
+    acute = "aeiouAEIOUyY",
+    grave = "aeiouAEIOU",
+    circunflex = "aeiouAEIOU",
+    tilde = "aoAOnN",
+    umlaut = "aeiouAEIOUy",
+    cedil = "cC"
+  )
+  
+  accentTypes <- c("´","`","^","~","¨","ç")
+  
+  if(any(c("all","al","a","todos","t","to","tod","todo")%in%pattern)) # opcao retirar todos
+    return(chartr(paste(symbols, collapse=""), paste(nudeSymbols, collapse=""), str))
+  
+  for(i in which(accentTypes%in%pattern))
+    str <- chartr(symbols[i],nudeSymbols[i], str) 
+  
+  return(str)
+}
+data$disciplina<-rm_accent(data$disciplina)
+data$disciplina<-toupper(data$disciplina)
+
+write_xlsx(data,"/Users/felipebarros/Documents/IPEA/PRONATEC/#208/carga_horaria.xslx")
+getwd()
+
